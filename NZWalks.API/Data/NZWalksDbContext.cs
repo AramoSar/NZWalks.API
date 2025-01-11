02bdc4bb3fec69f1fca791384d5e1ef0da6a88c0
Aram <aramsaribekyan1@gmail.com>
2025-01-11 09:28:40 +0400

    Adding Authorization and Authentication

--- NZWalks.API/Data/NZWalksDbContext.cs
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -5,7 +5,7 @@ namespace NZWalks.API.Data
 {
     public class NZWalksDbContext: DbContext
     {
-        public NZWalksDbContext(DbContextOptions dbContextoptions): base(dbContextoptions)
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> dbContextoptions): base(dbContextoptions)
         {
             
         }
@@ -13,6 +13,7 @@ namespace NZWalks.API.Data
         public DbSet<Difficulty> Difficulties { get; set; }
         public DbSet<Region> Regions { get; set; }
         public DbSet<Walk> Walks { get; set; }
+        public DbSet<Image> Images { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
